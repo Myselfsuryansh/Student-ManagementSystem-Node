f74b41ae5115d97a2371e9fb3d120d72e6368881
Myselfsuryansh <suryanshsinhabth11@gmail.com>
2024-07-31 21:49:26 +0530

    SSR for country dropdown

--- routes/bankRoutes.js
+++ routes/bankRoutes.js
@@ -5,20 +5,12 @@ const {
   getAllContriesName,
   getCitiesNameOfStates,
   getAllStateNameOfCountry,
-  getBankNameController,
-  getDistrictandStateController,
-  getDistrictNameController,
-  getStateNameController,
 } = require("../controllers/bankController");
 const router = express.Router();
 
 router.post("/registerBankDetails", addBankController);
 router.get("/getAllBankDetails", getBankController);
-router.get("/getSpecificBankDetails", getBankNameController);
-router.post("/getDistrictandState", getDistrictandStateController);
-router.get("/getStateName", getStateNameController);
-router.get("/getDistrictName/:stateID", getDistrictNameController);
 router.get("/getAllCountries", getAllContriesName);
-router.get("/getAllStatesOfCountries/:isoCode", getAllStateNameOfCountry);
-router.get("/getAllCitiesOfStates/:state_code", getCitiesNameOfStates)
+router.get("/getAllStatesOfCountries", getAllStateNameOfCountry);
+router.get("/getAllCitiesOfStates", getCitiesNameOfStates)
 module.exports = router;
