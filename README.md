b53093e5da45e74f6553c33fb1729d5a54ff6cb2
Myselfsuryansh <suryanshsinhabth11@gmail.com>
2024-07-31 17:12:25 +0530

    country-state JSON

--- README.md
+++
@@ -1,1 +0,0 @@
-Frontend code is in suryansh branch and Backend code is in main branch
--- routes/bankRoutes.js
+++ routes/bankRoutes.js
@@ -1,11 +1,23 @@
 const express = require("express");
-const {addBankController,getBankController,getBankNameController,getDistrictandStateController,getDistrictNameController ,getStateNameController} = require("../controllers/bankController");
+const {
+  addBankController,
+  getBankController,
+  getAllContriesName,
+  getAllStateNameOfCountry,
+  getBankNameController,
+  getDistrictandStateController,
+  getDistrictNameController,
+  getStateNameController,
+} = require("../controllers/bankController");
 const router = express.Router();
 
 router.post("/registerBankDetails", addBankController);
 router.get("/getAllBankDetails", getBankController);
-router.get("/getSpecificBankDetails",getBankNameController);
-router.post('/getDistrictandState',getDistrictandStateController);
-router.get("/getStateName",getStateNameController);
-router.get("/getDistrictName/:stateID",getDistrictNameController);
+router.get("/getSpecificBankDetails", getBankNameController);
+router.post("/getDistrictandState", getDistrictandStateController);
+router.get("/getStateName", getStateNameController);
+router.get("/getDistrictName/:stateID", getDistrictNameController);
+router.get("/getAllCountries", getAllContriesName);
+router.get("/getAllStatesOfCountries/:isoCode", getAllStateNameOfCountry);
+// router.get("/getAllCitiesOfStates/:id", getCitiesNameOfStates)
 module.exports = router;
